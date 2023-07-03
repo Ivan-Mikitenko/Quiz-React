2f806a60f0314e6da2ca03e8a7dc52361e0521d7
ivan <i.mikitenko@blanc.ru>
2023-07-04 00:18:18 +0500

    add

--- src/Result.js
+++ src/Result.js
@@ -5,7 +5,7 @@ function Result({correct}) {
     <div className="result">
       <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" alt={'ok'}/>
       <h2>Вы отгадали {correct} ответа из {questions.length}</h2>
-      <a href="/">
+      <a href="/Quiz-React">
         <button>Попробовать снова</button>
       </a>
     </div>
